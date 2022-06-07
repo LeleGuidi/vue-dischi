fa90f8f1f9166e21ff5a84e303506c07019fef0d
LeleGuidi <emanuele.guidi98@outlook.it>
2022-06-07 15:07:41 +0200

    Header completato

---
+++ src/components/blocks/BaseHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <header class="header">
+        <img src="../../assets/img/spotify-logo.png" alt="Logo Spotify" class="header_logo">
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+    background-color: var(--primary-color);
+    // Font size a 0 per rimuovere il bordo inferiore all'immagine
+    padding: .625rem 1.25rem;
+    font-size: 0;
+    &_logo {
+        width: 5rem;
+    }
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,14 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  chainWebpack: config => {
+    if (process.env.NODE_ENV === 'development') {
+      config
+        .output
+        .filename('[name].[hash].js') 
+        .end() 
+    }  
+  }
+}
